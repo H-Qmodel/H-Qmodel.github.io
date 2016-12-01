f161398283cc0972c65654e757ed9da131a8d649
H-Qmodel <1754519910@qq.com>
2016-12-01 16:07:14 +0800

    新增项目

---
+++ js/head.js
@@ -0,0 +1,26 @@
+$(function(){
+	$('#wangzhangdh').mouseover(function(){
+		$('.webdhbox').show();
+	});
+	$('#wangzhangdh').mouseout(function(){
+		$('.webdhbox').hide();
+	});
+});	
+
+$(function(){	
+	$('#headerUL_id2').mouseover(function(){
+		$('.headerUL_box_2').show();
+	});
+	$('#headerUL_id2').mouseout(function(){
+		$('.headerUL_box_2').hide();
+	});
+});		
+
+$(function(){
+	$('#headerUL_id3').mouseover(function(){
+		$('.headerUL_box_3').show();
+	});
+	$('#headerUL_id3').mouseout(function(){
+		$('.headerUL_box_3').hide();
+	});	
+});	
